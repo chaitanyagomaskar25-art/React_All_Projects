e2a16709d1ba26f1af3f60c27823e253fd7e86f2
Chaitanya Gomaskar <chaitanyagomaskar25@navgurukul.org>
2026-03-11 08:37:40 +0530

    This is learning project

---
+++ script.jsx
@@ -0,0 +1,35 @@
+// ============= 9/3/26 =====================
+
+// Import and export
+// import {b} from "./app.js"
+// console.log(b);
+
+
+// ============= 10/3/26 =====================
+// to  create element
+
+// let h1 = <h1>Hello</h1>
+
+// //  using function
+
+// function abc() {
+//     console.log(('hello'));
+    
+// }
+
+// // to call function
+// // abc()  
+// // or 
+// <abc />
+
+
+// Bundeler =>  its' basically a tool which helps to create folder (like vite)
+// command to craete folder => npm create-react-app (old way)
+// morden way => npm create vite (folder will make)
+// assets folder => used to keep images and videos , etc
+// public folder => used to keep static files which can be access publicly.
+// .eslint.config.js => it's helps to make your own custom rules. 
+// .gitignore => used to protect unwanted or secrete files from uploading on git hub
+// package.json => used to keep information of dependencies
+// package-lock.json => used to keep detailed information of dependencies
+// README.md => used to give information related to our project this is only for reading
